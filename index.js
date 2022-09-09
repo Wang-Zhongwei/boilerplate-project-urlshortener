04fdf742c81042d8ac0188e426574473fb85bd5f
Wang-Zhongwei <873994270@qq.com>
2022-09-09 16:16:38 -0400

    init commit

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+const longToShort = new Map();
+const shortToLong = new Map();
+const choices = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+const generateShortUrl = function() {
+    let shortUrl = '';
+    while (shortUrl.length < 1 || shortToLong.has(shortUrl)) {
+        shortUrl += choices[Math.floor(Math.random() * choices.length)];
+    }
+    return shortUrl;
+}
+
+app.post('/api/shorturl', function(req, res) {
+  let longUrl = req.body.url;
+  let shortUrl = null;
+  if (longToShort.has(longUrl)) {
+    shortUrl = longToShort.get(longUrl);
+  } else {
+    shortUrl = generateShortUrl();
+    longToShort.set(longUrl, shortUrl);
+    shortToLong.set(shortUrl, longUrl);
+  }
+  res.json({ original_url: longUrl, short_url: shortUrl });
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
